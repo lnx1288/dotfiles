531c5e26e1cc7b8adabf57ae562d7ee1d63f18b2
--global <--global>
2020-08-31 15:23:18 +0200

    Removed unnecessary apps from install script

--- scripts/app_install.sh
+++ scripts/app_install.sh
@@ -27,37 +27,11 @@ function snap_install {
 }
 
 # Basics
-install firefox
-install opera-stable
-install google-chrome-stable
-install clamav
-install clamtk
 install curl
-install git
 install htop
 install vim
-install virtualbox
-install virtualbox-ext-pack
-install gnome-tweak-tool
-install gnome-shell-extensions
-install gparted
-install vlc
-install remmina
-install timeshift
-install qemu
-install qemu-kvm
-install libvirt-bin
-install bridge-utils
-install virt-manager
-install conky-all
 
 # Snaps
-snap_install pac-vs
-snap_install wps-office
-snap_install wps-office-multilang
 snap_install cmadison
-
-# Others
-sudo snap install atom --classic
 
 #
