53319da48fe8cf3ce7cc0f5010beff9e6af05afe
lnx1288 <20599452+lnx1288@users.noreply.github.com>
2022-04-28 22:38:37 +0200

    CI: added full dotfile-based bootstrap test

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+DOTFILES_DIR="${HOME}/.dotfiles"
+
 # update and pre-requisites 
 sudo apt update
-sudo apt-get install -y ansible  
+sudo apt-get install -y ansible
+
+# clone dotfiles repo
+echo "Cloning dotfiles from ${REPO}, branch ${DEFAULT_BRANCH}"
+REPO="${REPO:-git@github.com:lnx1288/dotfiles.git}"
+git clone ${REPO} ${DOTFILES_DIR}
+cd ${DOTFILES_DIR}
 
 # run playbook(s)
-ansible-playbook --ask-become-pass --ask-vault-pass -i hosts main.yaml
+#ansible-playbook --ask-become-pass --ask-vault-pass -i hosts main.yaml
+ansible-playbook -i hosts main.yaml
