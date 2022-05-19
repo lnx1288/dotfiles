4791d8e443867227c7b2537fa861244a07bdbe2f
lnx1288 <20599452+lnx1288@users.noreply.github.com>
2022-05-19 17:47:11 +0200

    role shell: fixed not found package zsh-completions

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 sudo apt-get install -y ansible
 
 # run playbook(s)
+echo ""
+echo "Running ansible playbooks..."
+echo "============================"
 #ansible-playbook --ask-become-pass --ask-vault-pass -i hosts main.yaml
 ansible-playbook -i hosts main.yaml
 
